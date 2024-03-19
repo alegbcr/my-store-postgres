bcacf46a3e9fd7f3346fa42c44513757df595c7a
Alexander Gonzalez <alexgbcr@proton.me>
2024-03-18 19:29:50 -0600

    [UPDATE] change postgres confing to vercel

--- src/api/libs/postgres.pool.js
+++ src/api/libs/postgres.pool.js
@@ -1,8 +1,16 @@
-const {Pool} = require('pg');
-const {config} = require('../../../config/config');
+const { Pool } = require('pg');
+const { config } = require('../../../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`; 
+let URI = '';
 
-const pool = new Pool({ connectionString: URI })
+if (config.isProd) {
+  URI = config.dbUrl;
+} else {
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
+
+const pool = new Pool({ connectionString: URI });
+
+module.exports = pool;
