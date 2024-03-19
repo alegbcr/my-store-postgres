deec00d4793bff0182c88dadc03a0115f557da12
Alexander Gonzalez <alexgbcr@proton.me>
2024-03-19 00:32:38 -0600

    new change config

--- src/api/libs/sequelize.js
+++ src/api/libs/sequelize.js
@@ -2,7 +2,10 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../../../config/config');
 const setupModels = require('../db/models/index');
 
-const sequelize = new Sequelize(config.dbUrl);
+const sequelize = new Sequelize(config.dbUrl, {
+  dialect: 'postgres',
+  logging: false,
+});
 
 setupModels(sequelize);
 
